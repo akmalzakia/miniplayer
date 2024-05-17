310d3297c9de42a92cb8e6ca31265124292e9b32
Akmal Zaki <zaki.19051@mhs.its.ac.id>
2024-05-17 15:33:56 +0700

    Add playlist skeleton to featured page

---
+++ src/component/PlaylistCardSkeleton.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const LoadingDiv = styled.div`
+  background: linear-gradient(to right, #6b7280, #90959d, #6b7280);
+  background-size: 600% 600%;
+  animation: loading 3s ease infinite;
+
+  @keyframes loading {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+function PlaylistCardSkeleton({ imageOnly }) {
+  return (
+    <div
+      className={`flex flex-col p-2 overflow-hidden rounded-sm gap-2`}
+    >
+      <LoadingDiv className='max-w-full max-h-full  w-full pb-[100%] rounded-md'></LoadingDiv>
+      {!imageOnly && (
+        <>
+          <LoadingDiv className='font-bold text-base bg-gray-500 w-full h-4 rounded-lg'></LoadingDiv>
+          <LoadingDiv className='font-light text-sm bg-gray-500 w-1/2 h-[0.875rem] rounded-lg'></LoadingDiv>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default PlaylistCardSkeleton;
--- src/pages/Featured.jsx
+++ src/pages/Featured.jsx
@@ -2,9 +2,11 @@ import { useContext, useEffect, useState } from "react";
 import PlaylistCard from "../component/PlaylistCard";
 import { TokenContext } from "../context/tokenContext";
 import { spotifyAPI } from "../api/spotifyAxios";
+import PlaylistCardSkeleton from "../component/PlaylistCardSkeleton";
 
 function Featured() {
   const [featured, setFeatured] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const token = useContext(TokenContext);
   useEffect(() => {
     async function requestFeatured() {
@@ -14,16 +16,20 @@ function Featured() {
           locale: "en_EN",
           limit: 10,
           offset: 0,
-        }
-        const data = await spotifyAPI.getFeaturedPlaylists(token, params)
+        };
+        const data = await spotifyAPI.getFeaturedPlaylists(token, params);
         setFeatured(data.playlists.items);
+        setIsLoading(false);
       } catch (err) {
         console.log(err);
+        setIsLoading(true);
       }
     }
 
     if (featured.length === 0) {
       requestFeatured();
+    } else {
+      setIsLoading(false);
     }
   }, [featured, token]);
 
@@ -37,13 +43,15 @@ function Featured() {
           gridTemplateRows: "repeat(auto-fill, minmax(14em, 1fr))",
         }}
       >
-        {featured &&
+        {isLoading ? [...Array(6)].map((x, i) => <PlaylistCardSkeleton key={i}/>) : (
+          featured &&
           featured.map((playlist, idx) => (
             <PlaylistCard
               key={idx}
               playlist={playlist}
             ></PlaylistCard>
-          ))}
+          ))
+        )}
       </div>
     </>
   );
