fcb3fd45a70cb3c9e6b8370736253e0c7fc8045c
Akmal Zaki <zaki.19051@mhs.its.ac.id>
2024-08-08 18:24:31 +0700

    add infinite scroll in album search

--- src/pages/Search/SearchAlbums.tsx
+++ src/pages/Search/SearchAlbums.tsx
@@ -1,53 +1,35 @@
-import { useContext, useEffect, useState } from "react";
+import { useRef } from "react";
 import { useParams } from "react-router-dom";
-import { TokenContext } from "../../context/tokenContext";
-import { spotifyAPI } from "../../api/spotifyAxios";
 import GridDisplay from "../../component/GridDisplay";
 import { SpotifyObjectType } from "../../utils/enums";
+import useInfiniteSearch from "../../hooks/useInfiniteSearch";
 
 function SearchAlbums() {
   const { query } = useParams();
-  const token = useContext(TokenContext);
-  const [albums, setAlbums] =
-    useState<SpotifyApi.PagingObject<SpotifyApi.AlbumObjectSimplified> | null>(
-      null
-    );
-
-  useEffect(() => {
-    async function fetchAlbums(query: string) {
-      try {
-        const res = await spotifyAPI.search(
-          {
-            query,
-            type: ["album"],
-            limit: 30,
-          },
-          token
-        );
-
-        if (!res.albums) return;
-
-        setAlbums(res.albums);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    if (!query) return;
-
-    fetchAlbums(query);
-  }, [query, token]);
+  const triggerRef = useRef<HTMLDivElement>(null);
+  const albums = useInfiniteSearch<SpotifyApi.AlbumObjectSimplified>(
+    {
+      query: query ?? "",
+      type: "album",
+      limit: 30,
+    },
+    triggerRef.current ?? undefined
+  );
 
   return (
     <div>
       {albums && (
         <GridDisplay
-          data={albums.items}
+          data={albums}
           type={SpotifyObjectType.Album}
           isMulti={true}
           lazy
         />
       )}
+      <div
+        className='w-px h-px'
+        ref={triggerRef}
+      ></div>
     </div>
   );
 }
